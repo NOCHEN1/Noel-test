3e7018853d8fa955a8572d16691176f969385b83
NOCHEN1 <nchen6587@gmail.com>
2022-08-11 10:53:08 -0700

    Code for new menu

---
+++ Test Project/Assets/NewBehaviourScript.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("This is code for the menu");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
